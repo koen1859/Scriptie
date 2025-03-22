3884437097406a99646d42586c25240b98eee0af
koen1859 <koenstevens1859@gmail.com>
2025-03-22 21:19:57 +0100

    10-150 locations

--- project/main.py
+++ project/main.py
@@ -6,6 +6,8 @@ from graph import make_graph
 from nodes import get_nodes
 from read_tour import read_tours
 from tsp import parrallel_solve_tsps, create_tsps
+from map import create_map
+from route import paths_subset
 
 print("Importing the roads...")
 roads = get_road_data()
@@ -30,11 +32,11 @@ print(
     f"Graph has {len(graph.vs)} vertices and {len(graph.es)} edges. It contains {len(graph.components())} component(s)."
 )
 
-# print("Visualizing the road network on the map...")
-# create_map(roads, "Groningen.html")
+print("Visualizing the road network on the map...")
+create_map(roads, "Groningen.html")
 
 print("Creating TSP instances...")
-create_tsps(graph, 100, range(100, 110, 3), 8)
+create_tsps(graph, 10, range(10, 150, 2), 16)
 
 print("Solving the TSPs...")
 parrallel_solve_tsps(19)
@@ -42,8 +44,8 @@ parrallel_solve_tsps(19)
 print("Reading the output...")
 tours, distances = read_tours()
 
-# print("Visualizing some routes...")
-# paths_subset(graph, nodes, buildings, tours, distances)
+print("Visualizing some routes...")
+paths_subset(graph, nodes, buildings, tours, distances)
 
 print("Finding beta and making scatter plot...")
 x, y, b_hat, b = find_beta(distances)
