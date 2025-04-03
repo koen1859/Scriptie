abf81c89b709c30e65251c226cb72cd3bbf9e2b4
koen1859 <koenstevens1859@gmail.com>
2025-04-03 10:14:34 +0200

    test

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = [
-    (pkgs.python3.withPackages (ps: with ps; [
-      igraph
-      psycopg2
-      shapely
-      numpy
-      matplotlib
-      folium
-    ]))
+    (pkgs.python3.withPackages (ps:
+      with ps; [
+        igraph
+        psycopg2
+        shapely
+        numpy
+        matplotlib
+        folium
+      ]))
   ];
 }
