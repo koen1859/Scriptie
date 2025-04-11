7fc1302a2a8bac5967148a1c903e0fc564177b85
koen1859 <koenstevens1859@gmail.com>
2025-04-11 12:55:32 +0200

    simple dev flake setup

--- Create_DB.sh
+++ Create_DB.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-sudo -u postgres psql -c "CREATE DATABASE groningen_data;"
-sudo -u postgres psql -d groningen_data -c "CREATE EXTENSION postgis;"
-sudo -u postgres psql -d groningen_data -c "CREATE EXTENSION hstore"
-
 export PGUSER="postgres"
 export PGHOST="localhost"
 export PGPORT="5432"
 
-osm2pgsql --create --hstore --slim --multi-geometry --database groningen_data groningen-latest.osm.pbf
+for file in data/*.osm.pbf; do
+	# Extract base name
+	base=$(basename "$file" -latest.osm.pbf)
+
+	# Replace hyphens with underscores
+	dbname=${base//-/_}
+
+	echo "Creating database: $dbname"
+	sudo -u postgres psql -c "DROP DATABASE IF EXISTS $dbname;"
+	sudo -u postgres psql -c "CREATE DATABASE $dbname;"
+
+	echo "Enabling extensions on $dbname"
+	sudo -u postgres psql -d "$dbname" -c "CREATE EXTENSION postgis;"
+	sudo -u postgres psql -d "$dbname" -c "CREATE EXTENSION hstore;"
+
+	echo "Importing data from $file into $dbname"
+	osm2pgsql --create --hstore --slim --multi-geometry --database "$dbname" "$file"
+done
--- Create_DB2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PGUSER="postgres"
-export PGHOST="localhost"
-export PGPORT="5432"
-
-# osm2pgsql --create --hstore --multi-geometry --database groningen_data nodes_roads_buildings.osm.pbf
-osm2pgsql --create --hstore --slim --multi-geometry --database groningen_data groningen-latest.osm.pbf
