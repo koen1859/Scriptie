8582466754da2901f68da5b4c897a536ee42409e
koen1859 <koenstevens1859@gmail.com>
2025-04-11 12:35:06 +0200

    simple dev flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
   }:
@@ -23,32 +24,13 @@
           matplotlib
           folium
         ]);
-      pg = pkgs.postgresql_17;
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
           python-env
           pkgs.osm2pgsql
-          pkgs.gdal
-          pkgs.postgis
-          pkgs.postgresql_17
-          pkgs.postgresql17Packages.postgis
           (pkgs.callPackage ./lkh.nix {})
         ];
-
-        shellHook = ''
-          export PGDATA=$PWD/.pgdata
-          export PGPORT=5433
-          export PATH="$PGDATA/bin:$PATH"
-
-          if [ ! -d "$PGDATA" ]; then
-            ${pg}/bin/initdb -D "$PGDATA"
-            echo "local all all trust" > "$PGDATA/pg_hba.conf"
-            echo "host all all 127.0.0.1/32 trust" >> "$PGDATA/pg_hba.conf"
-            echo "host all all ::1/128 trust" >> "$PGDATA/pg_hba.conf"
-          fi
-          ${pg}/bin/pg_ctl -D "$PGDATA" -o "-p $PGPORT" -l "$PGDATA/logfile" start
-        '';
       };
     });
 }
