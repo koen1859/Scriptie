fc35667596a788e075f592015e1a2cfa8fad527b
koen1859 <koenstevens1859@gmail.com>
2025-04-18 18:37:10 +0200

    literature review

--- project/area.py
+++ project/area.py
@@ -1,6 +1,22 @@
-from shapely import union_all
+from shapely import union_all, concave_hull, convex_hull
 from shapely.geometry import Point, MultiPoint
+from shapely.wkt import loads
 import geopandas as gpd
+import matplotlib.pyplot as plt
+
+
+def plot_area(hull):
+    polygon = loads(hull)
+    x, y = polygon.exterior.xy
+    plt.figure(figsize=(10, 10))
+    plt.plot(x, y, color="blue")
+    plt.fill(x, y, alpha=0.3)
+    plt.title("Polygon Plot")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.axis("equal")
+    plt.grid(True)
+    plt.show()
 
 
 def get_area(buildings):
@@ -8,5 +24,6 @@ def get_area(buildings):
     gdf = gpd.GeoDataFrame(
         geometry=[Point(lon, lat) for lat, lon in points], crs="EPSG:4326"
     ).to_crs("EPSG:28992")
-    hull = union_all(gdf).convex_hull
+    hull = convex_hull(union_all(gdf))
+    # plot_area(str(hull))
     return hull.area / 1000000
